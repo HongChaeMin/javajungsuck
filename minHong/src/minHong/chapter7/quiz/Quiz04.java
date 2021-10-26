f9acc5e6952a1e434b7e8d09b876ad57b0e3b855
DESKTOP-47J45PU\jplus <ghdcoalss33@gmail.com>
2021-10-26 11:13:11 +0900

    [minHong] 7-5 문제 풀이

--- minHong/src/minHong/chapter7/quiz/Quiz04.java
+++ minHong/src/minHong/chapter7/quiz/Quiz04.java
@@ -4,6 +4,7 @@ class MyTv {
 	private boolean isPowerOn;
 	private int channel;
 	private int volume;
+	private int prevChannel;
 	
 	final int MAX_VOLUME = 100;
 	final int MIN_VOLUME = 0;
@@ -21,6 +22,7 @@ class MyTv {
 	}
 	public void setChannel(int channel) {
 		if (channel < MIN_CHANNEL || channel > MAX_VOLUME) return;
+		this.prevChannel = this.channel;
 		this.channel = channel;
 	}
 	public int getVolume() {
@@ -29,6 +31,11 @@ class MyTv {
 	public void setVolume(int volume) {
 		if (volume < MIN_VOLUME || volume > MAX_VOLUME) return;
 		this.volume = volume;
+	}
+	
+	// 7-5
+	public void gotoPrevChannel() {
+		setChannel(this.prevChannel); // tmp 역할
 	}
 }
 
@@ -42,6 +49,16 @@ public class Quiz04 {
 		t.setVolume(20);
 		System.out.println("VOL : " + t.getVolume());
 		
+		System.out.println();
+		// 7-5
+		t.setChannel(10);
+		System.out.println("CH : " + t.getChannel());
+		t.setChannel(20);
+		System.out.println("CH : " + t.getChannel());
+		t.gotoPrevChannel();
+		System.out.println("CH : " + t.getChannel());
+		t.gotoPrevChannel();
+		System.out.println("CH : " + t.getChannel());
 	}
 
 }
